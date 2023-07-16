505cc3469dcbe32786a036b5deb4762b3970f53b
Johnny Liao <johnnyliao06@gmail.com>
2023-07-15 19:07:08 -0700

    added form to allow components to be added to postgres and display on components page

--- nautobot_plugin/urls.py
+++ nautobot_plugin/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         path('components', views.ComponentsView.as_view(), name='components'),
+        path('add_component', views.add_component, name='add_component'),
         ]
--- nautobot_plugin/views.py
+++ nautobot_plugin/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
+from .forms import ComponentsForm
 from .models import Components
 
 class ComponentsView(View):
@@ -9,3 +10,17 @@ class ComponentsView(View):
         return render(request, 'nautobot_plugin/components.html', {
             'components': components
         })
+
+def add_component(request):
+    """
+    View for adding a new device.
+    """
+    if request.method == "POST":
+        form = ComponentsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/plugins/components/components")  # Redirect to the components view after successful form submission
+    else:
+        form = ComponentsForm()
+
+    return render(request, "nautobot_plugin/add_component.html", {"form": form})
